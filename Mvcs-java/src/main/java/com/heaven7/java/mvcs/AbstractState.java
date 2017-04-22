94bb6762b020feadaef58bf7ec297adb1587b8b3
LinghtSun <532278976@qq.com>
2017-04-22 18:31:40 +0800

    add a simple implements of AbstractState

--- Mvcs-java/src/main/java/com/heaven7/java/mvcs/AbstractState.java
+++ Mvcs-java/src/main/java/com/heaven7/java/mvcs/AbstractState.java
@@ -1,17 +1,33 @@
 package com.heaven7.java.mvcs;
 
+/**
+ * the abstract state which is controlled by {@link IController}
+ * @param <P> the state parameter type.
+ */
 public abstract class AbstractState<P extends StateParameter> {
 
 	private P mParam;
 
+	/**
+	 * get the state parameter.
+	 * @return the state parameter.
+     */
 	public P getStateParameter() {
 		return mParam;
 	}
 
+	/**
+	 * set the state parameter
+	 * @param p the parameter.
+     */
 	public void setStateParameter(P p) {
 		this.mParam = p;
 	}
-	
+
+	/**
+	 * merge the state parameter.
+	 * @param p the state parameter to merge
+     */
 	public void mergeStateParameter(P p){
 		if(p == null){
 			return;
@@ -39,6 +55,10 @@ public abstract class AbstractState<P extends StateParameter> {
 	 */
 	public abstract void onExit();
 
+	/**
+	 * called on update this state. often called by {@linkplain IController#notifyStateUpdate(StateParameter)}.
+	 * @param param the extra parameter.
+     */
 	public void onUpdate(P param) {
 	}
 }
---
+++ Mvcs-java/src/main/java/com/heaven7/java/mvcs/SimpleState.java
@@ -0,0 +1,23 @@
+package com.heaven7.java.mvcs;
+
+/**
+ * a adapter state of {@linkplain AbstractState}.
+ * Created by heaven7 on 2017/4/22.
+ */
+public abstract class SimpleState<P extends StateParameter> extends AbstractState<P> {
+
+    @Override
+    public void onEnter() {
+
+    }
+
+    @Override
+    public void onReenter() {
+
+    }
+
+    @Override
+    public void onExit() {
+
+    }
+}
