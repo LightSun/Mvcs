7b1fc96f0794d8a2e632ed5ac9afbdfd6ef2910e
LinghtSun <532278976@qq.com>
2017-05-01 08:53:28 +0800

    change doc ,try to add param check. @StateType but not need

--- Mvcs-java/src/main/java/com/heaven7/java/mvcs/util/MathUtil.java
+++ Mvcs-java/src/main/java/com/heaven7/java/mvcs/util/MathUtil.java
@@ -2,29 +2,43 @@ package com.heaven7.java.mvcs.util;
 
 /**
  * the math util
+ * 
  * @author heaven7
  *
  */
 public class MathUtil {
 
-	public static int log2n(int n){
+	public static int log2n(int n) {
 		return (int) (Math.log(n) / Math.log(2));
 	}
-	
+
 	/***
 	 * get the max 2^k . that make 2^k <= n
-	 * @param n the target
-	 * @return the max value(2^k) . which is the max value below n with value <= n.
+	 * 
+	 * @param n
+	 *            the target
+	 * @return the max value(2^k) . which is the max value below n with value <=
+	 *         n.
 	 */
-	public static int max2K(int n){
+	public static int max2K(int n) {
 		return (int) Math.pow(2, log2n(n));
 	}
-	
-	
-	/* public static void main(String[] args) {
-	    System.out.println(Math.log(Math.E));//1
-		System.out.println(Math.log(5) /Math.log(2)); //2
-		System.out.println(max2K(5)); //4
+
+	/*public static void main(String[] args) {
+		System.out.println(Math.log(Math.E));// 1
+		System.out.println(Math.log(5) / Math.log(2)); // 2
+		System.out.println(max2K(5)); // 4
 		System.out.println(max2K(1));
+		System.out.println(max2K(Integer.MAX_VALUE)); // 1073741824
+
+		int curFlags = 484359;
+		int maxKey;
+		for (; curFlags > 0;) {
+			maxKey = max2K(curFlags);
+			System.out.println("log2 maxKey = " + log2n(maxKey));
+			if (maxKey > 0) {
+				curFlags -= maxKey;
+			}
+		}
 	}*/
 }
